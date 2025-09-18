09e53f19e5d85dfc8bbfb6ded7b11e23af8bd247
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2025-09-18 15:08:45 +0200

    Light/dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Burger Menu _____________________________
 var burger = document.querySelector(".menu-burger");
 var menu = document.querySelector(".menu");
 
@@ -6,3 +7,18 @@ function toggleMenu() {
 }
 
 burger.addEventListener("click", toggleMenu);
+
+// Light/Dark mode___________________________
+
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
+
+// Filters____________________________________
