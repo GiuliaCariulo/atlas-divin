989c2612eaab89cca8203d6069ef538e986e15a1
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2026-03-13 11:09:12 +0100

    dynamic filters created

--- src/js/main.js
+++ src/js/main.js
@@ -37,3 +37,22 @@ toggleLightModeBtnMobile.addEventListener("click", function () {
 });
 
 // Filters____________________________________
+
+const cards = document.querySelectorAll(".god-card");
+const filters = document.querySelectorAll("input[name='type']");
+
+filters.forEach((filter) => {
+  filter.addEventListener("click", function () {
+    const value = filter.value; // Récupère la valeur du filtre cliqué
+
+    cards.forEach((card) => {
+      const type = card.getAttribute("data-type"); // Récupère la catégorie de la carte
+
+      if (type === value) {
+        card.classList.remove("hide"); // Montrer
+      } else {
+        card.classList.add("hide"); // Cacher
+      }
+    });
+  });
+});
